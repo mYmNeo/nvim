61faf8cf33ceeb784e9ced6aa4f6f07482590941
mYmNeo <thomassong2012@gmail.com>
2024-02-22 14:47:42 +0800

    update copilot-cmp configuration
    Signed-off-by: mYmNeo <thomassong2012@gmail.com>

--- lua/config/nvim-cmp.lua
+++ lua/config/nvim-cmp.lua
@@ -1,23 +1,92 @@
 local cmp = require("cmp")
 
+local kind_icons = {
+	Text = "",
+	Method = "m",
+	Function = "",
+	Constructor = "",
+	Field = "",
+	Variable = "",
+	Class = "",
+	Interface = "",
+	Module = "",
+	Property = "",
+	Unit = "",
+	Value = "",
+	Enum = "",
+	Keyword = "",
+	Snippet = "",
+	Color = "",
+	File = "",
+	Reference = "",
+	Folder = "",
+	EnumMember = "",
+	Constant = "",
+	Struct = "",
+	Event = "",
+	Operator = "",
+	TypeParameter = "",
+	Copilot = "",
+}
+
 cmp.setup({
-  sources = { -- Copilot Source
-    {
-      name = "nvim_lsp",
-      group_index = 1
-    },
-    {
-      name = "copilot",
-      group_index = 2
-    },
-    {
-      name = "path",
-      group_index = 2
-    },
-    {
-      name = "luasnip",
-      group_index = 2
-    }
-  },
-  mapping = require("keybinding/cmp").bindingOptions(cmp)
+	sources = { -- Copilot Source
+		{
+			name = "nvim_lsp",
+			priority = 40,
+		},
+		{
+			name = "path",
+			priority = 30,
+		},
+		{
+			name = "buffer",
+			priority = 20,
+		},
+		{
+			name = "luasnip",
+			priority = 10,
+		},
+		{
+			name = "copilot",
+			priority = 0,
+		},
+	},
+	mapping = require("keybinding/cmp").bindingOptions(cmp),
+	confirm_opts = {
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = false,
+	},
+	formatting = {
+		max_width = 80,
+		fields = { "kind", "abbr", "menu" },
+		format = function(entry, vim_item)
+			-- Kind icons
+			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+			vim_item.menu = ({
+				nvim_lsp = "[LSP]",
+				luasnip = "[Snippet]",
+				buffer = "[Buffer]",
+				path = "[Path]",
+				copilot = "[Copilot]",
+			})[entry.source.name]
+			return vim_item
+		end,
+	},
+	sorting = {
+		priority_weight = 2,
+		comparators = {
+			cmp.config.compare.offset,
+			cmp.config.compare.exact,
+			require("copilot_cmp.comparators").prioritize,
+			cmp.config.compare.score,
+			cmp.config.compare.recently_used,
+			cmp.config.compare.locality,
+			cmp.config.compare.kind,
+			cmp.config.compare.sort_text,
+			cmp.config.compare.length,
+			cmp.config.compare.order,
+		},
+	},
 })
