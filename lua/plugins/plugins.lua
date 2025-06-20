8bd040b4a81575658996399559c9d2ef580e51e3
thomassong <thomassong2012@gmail.com>
2025-06-20 13:15:22 +0800

    add blink plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,8 +1,5 @@
 return {
 	{
-		"hrsh7th/nvim-cmp",
-	},
-	{
 		"williamboman/mason.nvim",
 	},
 	{
@@ -89,5 +86,53 @@ return {
 		enabled = false,
 		event = { "BufReadPost", "BufNewFile" },
 		dependencies = { "kevinhwang91/promise-async" },
+	},
+	-- blink
+	{
+		"saghen/blink.cmp",
+		event = { "BufReadPost", "BufNewFile" },
+		version = "1.*",
+		dependencies = { "xzbdmw/colorful-menu.nvim", opts = {} },
+		opts = {
+			completion = {
+				documentation = {
+					auto_show = true,
+				},
+				menu = {
+					draw = {
+						columns = { { "kind_icon" }, { "label", gap = 1 } },
+						components = {
+							label = {
+								text = function(ctx)
+									return require("colorful-menu").blink_components_text(ctx)
+								end,
+								highlight = function(ctx)
+									return require("colorful-menu").blink_components_highlight(ctx)
+								end,
+							},
+						},
+					},
+				},
+			},
+			keymap = {
+				["<C-u>"] = { "scroll_documentation_up", "fallback" },
+				["<C-d>"] = { "scroll_documentation_down", "fallback" },
+			},
+			signature = {
+				enabled = true,
+			},
+			cmdline = {
+				completion = {
+					menu = {
+						auto_show = true,
+					},
+				},
+			},
+			sources = {
+				providers = {
+					snippets = { score_offset = 1000 },
+				},
+			},
+		},
 	},
 }
